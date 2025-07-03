e34d96e64f9319d146a8be59764910f1c3565325
Felix Boehme <git@felixboehme.dev>
2025-07-03 09:41:40 +0200

    refactor: avoid using autowired

--- src/main/java/org/example/spring/EventService.java
+++ src/main/java/org/example/spring/EventService.java
@@ -1,18 +1,18 @@
 package org.example.spring;
 
-import org.example.TicketShopClient;
-import org.example.TicketShopStringReader;
 import org.example.event.Event;
 import org.example.event.EventServiceInterface;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EventService {
-    @Autowired
-    private EventServiceInterface eventServiceInterface;
+    private final EventServiceInterface eventServiceInterface;
+
+    public EventService(EventServiceInterface eventServiceInterface) {
+        this.eventServiceInterface = eventServiceInterface;
+    }
 
     public List<Event> getAll() {
         return eventServiceInterface.getAll();
